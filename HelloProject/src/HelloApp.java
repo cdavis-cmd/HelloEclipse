732d5914a0f369b89f7823408ba4365c8d720901
cdavis-cmd <camdavispe@gmail.com>
2021-03-13 13:18:18 -0400

    Testing Commit and push to repository

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -1,7 +1,6 @@
 
 public class HelloApp {
-	public static void main(String args[]) {
-		System.out.println("Hello Cameron!");
-		System.out.println("Welcome Back");
+	public static void main(String[] args) {
+		System.out.println("Hello Cameorn");
 	}
 }
